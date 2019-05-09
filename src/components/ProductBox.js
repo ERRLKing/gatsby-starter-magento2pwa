120ff770608f11b97ad2d0b3c5b81edb582775df
ERRLKing <37886707+ERRLKing@users.noreply.github.com>
2019-05-09 18:34:34 -0500

    Update ProductBox.js

--- src/components/ProductBox.js
+++ src/components/ProductBox.js
@@ -20,7 +20,7 @@ class ProductBox extends React.Component {
                     render={data => (
                         <Link to={`/products/${this.props.handle}`}>
                             {this.props.images &&
-                                <img src={`${data.site.siteMetadata.backendUrl}/media/catalog/product/${this.props.images.url}`}
+                                <img src={`${data.site.siteMetadata.backendUrl}/media/catalog/product/${this.props.images}`}
                                     alt=""
                                     style={{
                                         maxWidth: '275px',
